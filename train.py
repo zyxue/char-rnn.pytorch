387aa08b43470ee00c373bab4130acf467adf2a7
Zhuyi Xue <alfred532008@gmail.com>
2018-06-01 10:06:32 -0700

    modualized parse_args

--- train.py
+++ train.py
@@ -13,21 +13,24 @@ from helpers import *
 from model import *
 from generate import *
 
-# Parse command line arguments
-argparser = argparse.ArgumentParser()
-argparser.add_argument('filename', type=str)
-argparser.add_argument('--model', type=str, default="gru")
-argparser.add_argument('--n_epochs', type=int, default=2000)
-argparser.add_argument('--print_every', type=int, default=100)
-argparser.add_argument('--hidden_size', type=int, default=100)
-argparser.add_argument('--n_layers', type=int, default=2)
-argparser.add_argument('--learning_rate', type=float, default=0.01)
-argparser.add_argument('--chunk_len', type=int, default=200)
-argparser.add_argument('--batch_size', type=int, default=100)
-argparser.add_argument('--shuffle', action='store_true')
-argparser.add_argument('--cuda', action='store_true')
-args = argparser.parse_args()
 
+def parse_args():
+    """Parse command line arguments"""
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('filename', type=str)
+    argparser.add_argument('--model', type=str, default="gru")
+    argparser.add_argument('--n_epochs', type=int, default=2000)
+    argparser.add_argument('--print_every', type=int, default=100)
+    argparser.add_argument('--hidden_size', type=int, default=100)
+    argparser.add_argument('--n_layers', type=int, default=2)
+    argparser.add_argument('--learning_rate', type=float, default=0.01)
+    argparser.add_argument('--chunk_len', type=int, default=200)
+    argparser.add_argument('--batch_size', type=int, default=100)
+    argparser.add_argument('--shuffle', action='store_true')
+    argparser.add_argument('--cuda', action='store_true')
+    return argparser.parse_args()
+
+args = parse_args()
 if args.cuda:
     print("Using CUDA")
 
@@ -63,7 +66,7 @@ def train(inp, target):
     loss.backward()
     decoder_optimizer.step()
 
-    return loss.data[0] / args.chunk_len.item()
+    return loss.item() / args.chunk_len
 
 def save():
     save_filename = os.path.splitext(os.path.basename(args.filename))[0] + '.pt'
