37f4b06505652351021022d99d2ec2094fbe36c3
Zhuyi Xue <alfred532008@gmail.com>
2018-06-01 10:00:47 -0700

    use tensor.item() to prevent warning for pytorch-0.5

--- train.py
+++ train.py
@@ -63,7 +63,7 @@ def train(inp, target):
     loss.backward()
     decoder_optimizer.step()
 
-    return loss.data[0] / args.chunk_len
+    return loss.data[0] / args.chunk_len.item()
 
 def save():
     save_filename = os.path.splitext(os.path.basename(args.filename))[0] + '.pt'
